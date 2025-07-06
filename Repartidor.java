b5cffa745a5b76a02bb40bf773fdf399ad8404ab
Franklin <frrosado@espol.edu.ec>
2025-07-06 00:47:14 -0500

    Actualizada la clase Repartidor con metodos nuevos, algunos aun no completados

--- Repartidor.java
+++ Repartidor.java
@@ -1,38 +1,5 @@
-public class Repartidor{
-    private String empresa;
-    private String email;
-    private String codigoRepartidor;
+package com.example.POO4_1P_MACHUCA_ROSADO_VINCES;
 
+public class Repartidor {
 
-    public Repartidor(String empresa, String email, String codigoRepartidor){
-        this.empresa=empresa;
-        this.email=email;
-        this.codigoRepartidor=codigoRepartidor;
-    }
-
-
-    public String getEmpresa(){
-        return empresa;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getCodigoRepartidor(){
-        return codigoRepartidor;
-    }
-
-
-    public void setEmpresa(String empresa){
-        this.empresa=empresa;
-    }
-
-    public void setEmail(String email){
-        this.email=email;
-    }
-
-    public void setCodigoRepartidor(String codigoRepartidor) {
-        this.codigoRepartidor = codigoRepartidor;
-    }
 }
