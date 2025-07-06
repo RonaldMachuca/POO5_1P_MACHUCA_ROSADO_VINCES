8fe371c63ef0f814975f99e2fbe41d65d6035847
Franklin <frrosado@espol.edu.ec>
2025-07-06 10:31:59 -0500

    Agregue una nueva clase Enum llamada CategoriaProducto donde se pondran las categorias de los diferentes productos

---
+++ EstadoProducto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.example.POO4_1P_MACHUCA_ROSADO_VINCES;
+
+
+/**
+ *
+ * @author user
+ */
+public enum EstadoProducto {
+    EN_PREPARACION, EN_RUTA, ENTREGADO
+}
