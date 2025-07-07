1abe2f993246c82a1e04793a9e4d3457f5186be9
Franklin <frrosado@espol.edu.ec>
2025-07-06 21:45:37 -0500

    Actualizada la clase EstadoProducto de acuerdo al proyeceto

--- EstadoProducto.java
+++ EstadoProducto.java
@@ -1,16 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-package com.mycompany.git;
+package com.funcionamiento;
 
-/**
- *
- * @author user
- */
 public enum EstadoProducto {
-    PENDIENTE,
-    ENVIADO,
-    ENTREGADO,
-    CANCELADO
+    EN_PREPARACION, EN_RUTA, ENTREGADO
 }
